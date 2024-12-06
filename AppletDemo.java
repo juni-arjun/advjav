a5d98621201013addd32a239b14d5d34bec13955
Arjun Singh Chouhan <147067822+juni-arjun@users.noreply.github.com>
2024-12-06 12:52:40 +0530

    Delete AppletDemo.java

--- AppletDemo.java
+++
@@ -1,8 +0,0 @@
-import java.applet.*;
-import java.awt.*;
-public class AppletDemo extends Applet{
-    public void paint(Graphics g){
-        g.drawString("HelloApplet", 100, 300);
-        g.drawRect(100, 100, 300, 300);
-    }
-}
